4a8cbac8fb6cc2139e77977fb06b25d5fb43de03
Kaden Scroggins <kadenscroggins@gmail.com>
2023-10-27 13:58:34 -0500

    Variables P2 challenge (BABY MODE)

--- Scripts/Scrippy.gd
+++ Scripts/Scrippy.gd
@@ -1,16 +1,16 @@
 extends Node2D
 
-var score: int = 5
-var move_speed: float = 2.53
-var game_over: bool = false
-var ability: String = "slash"
+var country_name: String = "Frogstead"
+var population: int = 43153986
+var altitude_max: float = 2002.54
+var landlocked: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	game_over = true
-	print(game_over)
-	ability = "heal"
-	print(ability)
+	print("Country name: %s" % country_name)
+	print("Population: %d" % population)
+	print("Maximum altitude: %f" % altitude_max)
+	print("Is landlocked: %s" % landlocked)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
