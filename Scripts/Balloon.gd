ef9e614d328e5c14dbec3a47efc0a8358a7e7de1
Kaden Scroggins <kadenscroggins@gmail.com>
2023-11-03 14:50:07 -0500

    Balloon Popper tutorial part 3

--- Scripts/Balloon.gd
+++ Scripts/Balloon.gd
@@ -1,6 +1,6 @@
 extends Area3D
 
-var clicks_to_pop: int = 3
+var clicks_to_pop: int = 5
 var size_increase: float = 0.2
 var score_to_give: int = 1
 
@@ -8,4 +8,9 @@ func _on_input_event(camera, event, position, normal, shape_idx):
 	if event is InputEventMouseButton \
 			 and event.button_index == MOUSE_BUTTON_LEFT \
 			 and event.pressed: # Only trigger on mouse click, not release
-		print("Increase size")
+		scale += Vector3.ONE * size_increase
+		clicks_to_pop -= 1
+
+	if clicks_to_pop <= 0:
+		get_node("/root/Oxygen").increase_score(score_to_give)
+		queue_free()
