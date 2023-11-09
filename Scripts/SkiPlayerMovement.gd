c10d2525c69d8e5f441aa412e9f85139dce78f4f
Kaden Scroggins <kadenscroggins@gmail.com>
2023-11-08 23:12:57 -0600

    Collision stuff part 1 and 2

---
+++ Scripts/SkiPlayerMovement.gd
@@ -0,0 +1,9 @@
+extends RigidBody3D
+
+@export var move_spoed: float = 2.0
+
+func _physics_process(delta):
+	if Input.is_key_pressed(KEY_LEFT):
+		linear_velocity.x = -move_spoed
+	elif Input.is_key_pressed(KEY_RIGHT):
+		linear_velocity.x = move_spoed
